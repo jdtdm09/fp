2a71f1dbd46ff312e415c5522299869c8a6a4137
Diogo Magalhães <josedmagalhaes100809@gmail.com>
2023-10-24 22:13:28 +0100

    parte2

---
+++ Ficha Prática 2 - Parte 2/main.c
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/main.c to edit this template
+ */
+
+/* 
+ * File:   main.c
+ * Author: Wrt
+ *
+ * Created on 24 de outubro de 2023, 22:12
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ Ficha Prática 2 - Parte 2/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=ficha_pr_tica_2_-_parte_2
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/ficha_pr_tica_2_-_parte_2
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=fichaprática2-parte2.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/fichaprática2-parte2.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=ficha_pr_tica_2_-_parte_2
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/ficha_pr_tica_2_-_parte_2
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=fichaprática2-parte2.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/fichaprática2-parte2.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
